202809e9a2777cb9ca666b0aabc32190387fc20f
DWEMMANUELANDRADA <74630281+DWEMMANUELANDRADA@users.noreply.github.com>
2021-08-13 21:15:37 -0400

    Update efectos.js

--- js/efectos.js
+++ js/efectos.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
  
-
+let jeans= document.querySelectorAll("#jeans");
+  function mostrarScroll(){
+    let scrollTop= document.documentElement.scrollTop;
+    for (var i = 0 ; i < jeans.length; i++){
+      let altura = jeans[i].offsetTop;
+      if(altura - 200 < scrollTop){
+        jeans[i].style.opacity=1;
+      }
+    }
+  }
+  window.addEventListener("scroll", mostrarScroll);
   $(".ak").on(
     {mouseenter:function(){
       $(this).css("opacity",".5");
